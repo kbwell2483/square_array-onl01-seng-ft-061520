feb445c6cbbc485641bf77d0bb4d49e4514e8eb6
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-22 17:36:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   numbers = ["1", "2", "3"]
   
   numbers.each do |square|
-    sum = "#{square} * #{square}"
-    print sum 
+    puts "#{square} * #{square
+    gets.chomp
+   
 end
 end
