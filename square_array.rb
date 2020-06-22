aa4260eea1b450189c560695a04b1b6aaa45b6e0
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-22 18:37:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  numbers = ["1", "2", "3"]
+  numbers = [1, 2, 3]
   
   numbers.each do |square|
-    squared = square * square
-    squared_numbers = []
-    squared_numbers << squared
+    puts square * square
+    
+    
 end
-  squared_numbers
+   
 end
